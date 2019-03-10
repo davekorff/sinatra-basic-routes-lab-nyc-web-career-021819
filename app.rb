71a423ec96e2d8d6c433113f316a6a8b38d045e6
David Korff <dkorff12@gmail.com>
2019-03-10 14:08:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Inigo"
+  end
+
+  get '/hometown' do
+    "My hometown is Florin City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Marcha Real"
+  end
 end
